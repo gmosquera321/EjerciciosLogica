070bf3e9656373921b9cd9242440ba9b5c6e1855
Gisella Hurtado Mosquera <ghurtadomosquera504@gmail.com>
2023-09-21 18:19:44 -0500

    solucion

--- ejercico6/ejercico6/Program.cs
+++ ejercico6/ejercico6/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Realizar un programa en C# que obtenga el promedio de calificaciones de un grupo 
+//de N alumnos
+
+public class ejercicio6
+{
+    static void Main(string[] args)
+    {
+        Console.Write("ingrese la cantidad de alumnos:");
+
+        int cantidadAlumnos = int.Parse(Console.ReadLine());
+
+        double sumaCalificcaciones = 0;
+
+        for (int i = 0; i <= cantidadAlumnos; i++) ;
+        {
+            Console.Write("ingrese la cantidad de alumnos{i}:");
+
+            double calificacion = double.Parse(Console.ReadLine());
+
+            sumaCalificcaciones += calificacion;
+
+        }
+
+        double promedio = sumaCalificcaciones / cantidadAlumnos;
+
+        Console.Write($"el promedio de calificaciones es {promedio}");
+
+
+
+
+
+    }
+
+}
